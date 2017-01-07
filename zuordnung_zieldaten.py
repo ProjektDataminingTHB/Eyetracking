34b0781ca83d2051ecbdeab8a58998956a18df0a
MentalistHB <hervalnganya@yahoo.fr>
2017-01-07 14:38:26 +0100

    visualisierung, gitignore

--- zuordnung_zieldaten.py
+++ zuordnung_zieldaten.py
@@ -52,7 +52,6 @@ for dateiname in dateinamen:
 			messung_verzeichnisse = task_verzeichnisse + "/" + dateiname3
 			dateinamen4 = os.listdir(messung_verzeichnisse)
 			for dateiname4 in dateinamen4:
-<<<<<<< HEAD
 				cycle_verzeichnisse = messung_verzeichnisse + "/" + dateiname4
 				dateinamen5 = os.listdir(cycle_verzeichnisse)
 				for dateiname5 in dateinamen5:
@@ -84,33 +83,3 @@ for dateiname in dateinamen:
 											np.array([ergebnis_spalte3]).T), axis = 1)
 					df = pd.DataFrame(data, columns=header_target)
 					df.to_csv(messung_verzeichnisse + '/' + target_csv, index=False)
-=======
-				messungen_csv = messung_verzeichnisse + "/" + dateiname4
-				messung_zeitstempel = list(pd.read_csv(messungen_csv, sep = ',')['zeitstempel'])
-				messung_zeitstempel = list(messung_zeitstempel[1:])
-				erster_zeitwert = messung_zeitstempel[0]
-				# das entfernt zwar den letzten Punkt in der Liste, allerdings benötigen wir die Liste auch nicht weiter
-				letzter_zeitwert = messung_zeitstempel.pop()
-				print(messungen_csv)
-				print('erster Wert' + str(erster_zeitwert))
-				erster_wert = finde_ersten_wert(ziel_zeitstempel, erster_zeitwert)
-				letzter_wert = finde_letzten_wert(ziel_zeitstempel, letzter_zeitwert, erster_wert)
-				print(erster_wert)
-				print(letzter_wert)
-				ergebnis_spalte1 = list(csv_df['t_tracker'])
-				ergebnis_spalte2 = list(csv_df['pix_x'])
-				ergebnis_spalte3 = list(csv_df['pix_y'])
-
-				beginn = ergebnis_spalte1.index(erster_wert)
-				ende = ergebnis_spalte1.index(letzter_wert) + 1
-				
-				ergebnis_spalte1 = list(ergebnis_spalte1[beginn:ende])
-				ergebnis_spalte2 = list(ergebnis_spalte2[beginn:ende])
-				ergebnis_spalte3 = list(ergebnis_spalte3[beginn:ende])
-				
-				data = np.concatenate((np.array([ergebnis_spalte1]).T,
-                                      np.array([ergebnis_spalte2]).T,
-                                        np.array([ergebnis_spalte3]).T), axis = 1)
-				df = pd.DataFrame(data, columns=header_target)
-				df.to_csv(messung_verzeichnisse + '/' + target_csv, index=False)
->>>>>>> 69bcb79edbc994eaf40d683cdfd48bc89768a1cb
