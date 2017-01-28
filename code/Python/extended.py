c1d20272746413eea641f92fc75c7d8e6fe3932b
kaulima19 <kaulima19@gmail.com>
2017-01-28 16:05:20 +0100

    Berechnung der Verschobenen und an der x-Achse gespiegelten Targetpunktpositionen
    Anpassung der Berechnung des abstands an die verschobenen Punkte.

--- code/Python/extended.py
+++ code/Python/extended.py
@@ -23,8 +23,9 @@ def extend_files():
                 copyfile(os.path.join(source_path, source_file), os.path.join(destination_path, source_file))
             else:
                 source = pd.read_csv(os.path.join(source_path, source_file), sep=',', names = header_source).ix[1:]
-                middle_eyes = source.assign(blick_m_x = pd.to_numeric(source.blick_l_x) / 2 + pd.to_numeric(source.blick_r_x) / 2, blick_m_y = pd.to_numeric(source.blick_l_y) / 2 + pd.to_numeric(source.blick_r_y) / 2)
-                result = middle_eyes.assign(delta_l_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_l_x) - pd.to_numeric(x.pix_x),2) + np.power(pd.to_numeric(x.blick_l_y) - pd.to_numeric(x.pix_y),2)), delta_r_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_r_x) - pd.to_numeric(x.pix_x),2) + np.power(pd.to_numeric(x.blick_r_y) - pd.to_numeric(x.pix_y),2)), delta_m_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_m_x) - pd.to_numeric(x.pix_x),2) + np.power(pd.to_numeric(x.blick_m_y) - pd.to_numeric(x.pix_y),2)))
+                middle_eyes = source.assign(blick_m_x = pd.to_numeric(source.blick_l_x) / 2 + pd.to_numeric(source.blick_r_x) / 2, blick_m_y = pd.to_numeric(source.blick_l_y) / 2 + pd.to_numeric(source.blick_r_y) / 2, pix_x_translation = pd.to_numeric(source.pix_x).add(640), pix_y_translation = (pd.to_numeric(source.pix_y) * -1).add(512))
+                
+                result = middle_eyes.assign(delta_l_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_l_x) - pd.to_numeric(x.pix_x_translation),2) + np.power(pd.to_numeric(x.blick_l_y) - pd.to_numeric(x.pix_y_translation),2)), delta_r_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_r_x) - pd.to_numeric(x.pix_x_translation),2) + np.power(pd.to_numeric(x.blick_r_y) - pd.to_numeric(x.pix_y_translation),2)), delta_m_t = lambda x : np.sqrt(np.power(pd.to_numeric(x.blick_m_x) - pd.to_numeric(x.pix_x_translation),2) + np.power(pd.to_numeric(x.blick_m_y) - pd.to_numeric(x.pix_y_translation),2)))
 
                 result.to_csv(os.path.join(destination_path, source_file), index=False)
 
